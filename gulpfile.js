20ab6e4183b3b5ed7e8b24c6a9273f11d97bacd2
taaem <hipsteeeer@web.de>
2015-03-02 12:01:44 +0100

    Changed to use SMACSS for the styles

--- gulpfile.js
+++ gulpfile.js
@@ -9,14 +9,14 @@ gulp.task('styles', function() {
   gulp.src('./styles/main.styl')
     .pipe(stylus())
     .pipe(gulp.dest('./dist/css/'));
-})
+});
 
 gulp.task('public', function () {
   gulp.src('public/**/*')
-  .pipe(gulp.dest('./dist/'))
-})
+  .pipe(gulp.dest('./dist/'));
+});
 
 gulp.task('watch', function() {
-  gulp.watch('styles/*.styl', ['styles']);
-  gulp.watch('public/**/*', ['public'])
-})
+  gulp.watch('styles/**/*.styl', ['styles']);
+  gulp.watch('public/**/*', ['public']);
+});
--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,14 @@
 $(document).ready(function () {
   $('a').smoothScroll();
-  $(".name").flipping_text({
+  $(".title").flipping_text({
     tickerCount: 15,
     opacityEffect: true,
     resetOnChange: true
   });
-  $(".motto").flipping_text({
+  $(".title--text").flipping_text({
     tickerTime: 8,
     tickerCount: 8,
     resetOnChange: true
   });
   
-})
+});
